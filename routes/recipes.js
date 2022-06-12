92e4fc2e2cc1b10185c79b73af17772893ca2875
meitii1212 <meitalgl@post.bgu.ac.il>
2022-06-12 16:49:42 +0300

    to check

--- routes/recipes.js
+++ routes/recipes.js
@@ -34,11 +34,11 @@ router.get("/searchRecipe", async (req, res, next) => {
   try {
 
     const recipes = await recipes_utils.getSearchResults(req.query.query,req.query.titleMatch,req.query.number,req.query.cuisine,req.query.diet,req.query.intolerances)
-    // if (recipes==[]){
-    //   throw { status: 204, message: "No results" };
-    // }
+    if (recipes.length==0){
+      throw { status: 204, message: "No results" };
+    }
     
-    
+  
     //res.status(200).send({ message: "results returned", success: true });
     res.send(recipes);
     
